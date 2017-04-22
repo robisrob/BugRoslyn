a014b0edebda7bbdb5eec99d7bb54e71bf7ee5ab
robs <rob.swartenbroekx@cegeka.com>
2017-04-22 17:49:47 +0200

    broke build with strange roslyn error

---
+++ Test.cs
@@ -0,0 +1,28 @@
+﻿namespace BugRosLyn
+{
+    public class Test
+{
+    private int _field;
+
+        public void OuterMethod(int outerParam)
+        {
+            void InnerMethod1()
+            {
+                void InnerInnerMethod(int innerInnerParam)
+                {
+                    InnerInnerInnerMethod();
+                    
+                    bool InnerInnerInnerMethod() {
+                        return innerInnerParam != _field;
+                    }
+                }
+
+                void InnerMethod2()
+                {
+                    var temp = outerParam;
+                }  
+        }
+    }
+}
+
+}
